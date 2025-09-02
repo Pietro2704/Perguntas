fe40f57cf1cd60f9915576f8faa3e650db049417
Pietro <pietro.rosolia.2704@gmail.com>
2025-09-01 22:22:58 -0300

    varias coisas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const div_pergunta = document.querySelector("#pergunta")
+const div_opcoes = document.querySelector("#opcoes")
+const div_feedback = document.querySelector("#feedback")
+const nextButton = document.querySelector("#nxtBtn")
+
 const perguntas = [
     {
         pergunta: "Qual a capital da França?",
@@ -10,3 +15,27 @@ const perguntas = [
         resposta: 3
     }
 ]
+
+let indicePerguntas = 0
+
+function mostrarPergunta(){
+    // Padrões para mostrar pergunta
+    div_feedback.innerHTML = ""
+    nextButton.classList.add("d-none")
+
+    // Colocar Pergunta no HTML
+    let p = perguntas[indicePerguntas]
+    div_pergunta.textContent = p.pergunta
+
+    // Opcoes
+    div_opcoes.innerHTML = ""
+    p.opcoes.forEach((opcao, i) => {
+        const btn = document.createElement("button")
+        btn.className = "btn btn-outline-primary"
+        btn.textContent = opcao
+        btn.addEventListener('click', () => verificarResposta(i))
+        div_opcoes.appendChild(btn)
+    })
+}
+
+mostrarPergunta()
