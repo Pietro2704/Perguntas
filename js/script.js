428c053ec85a9173dc3f53c773dd2b41e7501d97
Pietro <pietro.rosolia.2704@gmail.com>
2025-09-01 21:43:22 -0300

    perguntas

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const perguntas = [
+    {
+        pergunta: "Qual a capital da França?",
+        opcoes: ["Paris", "Roma", "Berlim", "Madrid"],
+        resposta: 0
+    },
+    {
+        pergunta: "Qual tag HTML cria um parágrafo?",
+        opcoes: ["<div>", "<span>", "<h6>", "<p>"],
+        resposta: 3
+    }
+]
